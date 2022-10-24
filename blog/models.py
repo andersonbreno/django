7c352fea4d7582786dac5100573ed9e86d6a5429
anderson <andersonbrenotec@gmail.com>
2022-10-24 10:31:34 -0300

    Mais um projeto de blog para treinar.

---
+++ blog/models.py
@@ -0,0 +1,23 @@
+from random import choices
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Post(models.Model):
+    
+    POST_STATUS = (
+        ('active', 'ativo'),
+        ('draft', 'Rascunho'),
+    )
+    title = models.CharField(max_length=200)
+    body = models.TextField()
+    # slug = models.SlugField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateField(auto_now_add=True) #ao adicionar o post é criada uma data no banco.
+    updated_at = models.DateField(auto_now=True) # sempre que o post/item for salvo a data é atualizada automáticamente.
+    status = models.CharField(max_length=15, choices=POST_STATUS)
+
+
+
+def __str__(self):
+    return self.title        
